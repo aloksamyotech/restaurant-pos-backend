2823a310195cf244f4377820c0f609e6a0bff24b
amansamyotech <aman.asati@samyotech,com>
2025-04-16 18:10:50 +0530

    all final code with ci cd

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ import path from "path";
 const app = express();
 const PORT = (() => {
   const env = process.env.ENV;
-  return env === "development" ? 7002 : 7002;
+  return env === "development" ? 7001 : 7001;
 })();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
